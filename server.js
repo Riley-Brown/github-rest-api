d8bdaf227275534ab0f9bb1478bc9556ec5b59f9
Riley-Brown <riley@riley.gg>
2019-01-02 01:29:26 -0500

    setup api with env keys for deployment

--- server.js
+++ server.js
@@ -5,6 +5,12 @@ const axios = require('axios');
 
 const app = express();
 
+app.get('/', (req, res) => {
+  const api = axios.get(`https://api.github.com/users/Riley-Brown/repos?per_page=4&sort=created:asc&client_id=5339e254177ffa590980&client_secret=${process.env.CLIENT_SECRET}`)
+    .then(response => {
+      const data = response.data;
+      res.send(JSON.stringify(data))
+    })
+})
 const port = process.env.PORT || 9000;
-
 app.listen(port, () => console.log(`server running on ${port}`))
